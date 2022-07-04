8f76ebd69ec70b9edb1211b7e3421a63da7b085f
cgill91 <coltongillmore09@gmail.com>
2022-07-04 17:21:59 -0500

    finished api work

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -15,11 +15,16 @@ class Playlist extends React.Component {
     render() {
         return(
             <div className="Playlist">
-                <input defaultValue={"New Playlist"}
-                onChange={this.handleNameChange}/>
-                 <TrackList tracks={this.props.playlistTracks}
+                <input 
+                defaultValue={"New Playlist"}
+                onChange={this.handleNameChange}
+                />
+                 <TrackList 
+                 tracks={this.props.playlistTracks}
+                 onAdd={this.props.onAdd}
                  onRemove={this.props.onRemove}
-                 isRemoval={true}/>
+                 isRemoval={true}
+                 />
                  <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
         )
