2507a9ec4a41e827d37a981ef33d349db18b9158
cgill91 <coltongillmore09@gmail.com>
2022-07-04 18:28:07 -0500

    Updates

---
+++ precache-manifest.bcbd0d4dc8fe18b89ec9944c76fb8270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc7240aaa338a5391a83",
+    "url": "/Jamming/static/css/main.fd0b738e.chunk.css"
+  },
+  {
+    "revision": "cc7240aaa338a5391a83",
+    "url": "/Jamming/static/js/main.cc7240aa.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/Jamming/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "7f1c9c883172072c0e51",
+    "url": "/Jamming/static/js/2.7f1c9c88.chunk.js"
+  },
+  {
+    "revision": "687ca55a66d8284ec69c",
+    "url": "/Jamming/static/js/runtime~main.687ca55a.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jamming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "50ab6976deaf94294efbdbf34876822c",
+    "url": "/Jamming/index.html"
+  }
+];
